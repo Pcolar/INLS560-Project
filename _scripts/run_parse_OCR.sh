5bbeac0793bcd162e9ce183d33bfdca3eec4ba4b
Dave Pcolar <pcolar@email.unc.edu>
2013-12-04 16:03:45 -0500

    changes to run script to fix issues with find traversing all subdirectories

--- _scripts/run_parse_OCR.sh
+++ _scripts/run_parse_OCR.sh
@@ -2,8 +2,6 @@
 #set -x
 export Parse_Limit=15
 export CSV_output_file="testfile.csv"
-#export OCR_File="/home/pcolar/OCR-Parser/_data/dailytarheel_june15_1946_dec12_1946_0001.txt"
+#export OCR_File="~/OCR-Parser/_data/dailytarheel_june15_1946_dec12_1946_0001.txt"
 
-#find $1 -type f -name *.txt 
-
-ls --width=50 $1 | python ~/OCR-Parser/_regex/parse_OCR_file.py
+find $1 -maxdepth 1 -type f  -name *.txt | python ~/OCR-Parser/_regex/parse_OCR_file.py
